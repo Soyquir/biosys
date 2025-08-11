eb81eac0b4ef64f591c8fa488316b05066390844
Juan Jesus Quiroga Tosca <quirogatoscajuan@gmail.com>
2025-08-11 13:13:25 -0600

    Principal finalizado

--- principal.py
+++ principal.py
@@ -1,20 +1,90 @@
 import flet as ft
-def main (page:ft.Page):
+import alta_usuario as al
+import consulta_usuario as cu
+import alta_bioenergia as alb
+import consulta_bio as cbio
 
-    def mostrar_registro(e:ft.ControlEvent ):
+def main(page: ft.Page):
+    
+    def mostrar_registro(e):
         page.clean()
-        
-    page.title="menu principal"
-    page.theme_mode="light"
-    page.appbar=ft.AppBar(
-        title=ft.Text ("SISTEMA DE GESTION DE ENERGIAS"),
-        leading=ft.Icon("energy_savings_leaf"),
+        al.main(page)
+
+    def consultar_usuario(e):
+        page.clean()
+        cu.main(page)
+
+    def agregar_bioenergia(e):
+        page.clean()
+        alb.main(page)
+
+    def consultar_bioenergia(e):
+        page.clean()
+        cbio.main(page)
+
+    # Configuración de la página
+    page.title = "Menú principal"
+    page.theme_mode = "light"
+    page.appbar = ft.AppBar(
+        title=ft.Text("Sistema de gestión de bioenergías", size=20, weight="bold"),
+        leading=ft.Icon(ft.Icons.ENERGY_SAVINGS_LEAF),
         color="white",
-        bgcolor="purple"
+        bgcolor=ft.Colors.GREEN_800 
     )
 
-    btn_registro=ft.ElevatedButton("registro")
-    btn_consultas=ft.ElevatedButton("consultas")
-    page.add(btn_registro,btn_consultas)
-    page.update()
-ft.app(target=main)
+    btn_registro = ft.ElevatedButton(
+        "Registro de usuario",
+        icon=ft.Icons.PERSON_ADD,
+        bgcolor=ft.Colors.LIGHT_GREEN_500, 
+        color="white",
+        style=ft.ButtonStyle(padding=20),
+        on_click=mostrar_registro
+    )
+    btn_consultas = ft.ElevatedButton(
+        "Consulta de usuario",
+        icon=ft.Icons.SEARCH,
+        bgcolor=ft.Colors.CYAN_500,
+        color="white",
+        style=ft.ButtonStyle(padding=20),
+        on_click=consultar_usuario
+    )
+    btn_nueva = ft.ElevatedButton(
+        "Registro de bioenergía",
+        icon=ft.Icons.ADD_CIRCLE,
+        bgcolor=ft.Colors.AMBER_500,
+        color="white",
+        style=ft.ButtonStyle(padding=20),
+        on_click=agregar_bioenergia
+    )
+    btn_bionergia = ft.ElevatedButton(
+        "Consulta de bioenergía",
+        icon=ft.Icons.LIST_ALT,
+        bgcolor=ft.Colors.DEEP_PURPLE_500,
+        color="white",
+        style=ft.ButtonStyle(padding=20),
+        on_click=consultar_bioenergia
+    )
+
+    page.add(
+        ft.Container(
+            content=ft.Column(
+                [
+                    ft.Text("Menú Principal", size=30, weight="bold", color="Green"),
+                    ft.Divider(height=20, thickness=2, color="Green"),
+                    btn_registro,
+                    btn_consultas,
+                    btn_nueva,
+                    btn_bionergia
+                ],
+                spacing=20,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER
+            ),
+            alignment=ft.alignment.center,
+            padding=50,
+            expand=True
+        )
+    )
+
+# Iniciar app
+if __name__ == "__main__":
+    ft.app(target=main)
